98345ee68dac046baabab912edc962860e8e4cfb
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2022-12-07 21:00:43 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             #'throttle:api',
-            'throttle:60000,1',
+            #'throttle:60000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
